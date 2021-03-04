e19b72e8cfc496ebe5471984d17ca643641030c2
Thomas Witherow <twitherow@bellsouth.net>
2021-03-04 11:19:56 -0600

    got doodles back

--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -29,6 +29,14 @@ export const AnimalProvider = (props) => {
         .then(getAnimals)
     }
 
+    const releaseAnimal = animalId => {
+        return fetch(`http://localhost:8088/animals/${animalId}`, {
+            method: "DELETE"
+        })
+            .then(getAnimals)
+    }
+
+
     /*
         You return a context provider which has the
         `animals` state, `getAnimals` function,
@@ -37,7 +45,7 @@ export const AnimalProvider = (props) => {
     */
     return (
         <AnimalContext.Provider value={{
-            animals,  addAnimal, getAnimals, getAnimalById
+            animals,  addAnimal, getAnimals, getAnimalById, releaseAnimal
         }}>
             {props.children}
         </AnimalContext.Provider>
